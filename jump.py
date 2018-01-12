8e8109e4d67761d29a6f44bd9c881705422ecc3f
bradl406 <bradl406@umn.edu>
2018-01-11 21:56:17 -0600

    updated constructor and created main function

--- jump.py
+++ jump.py
@@ -1,3 +1,15 @@
 import unicornhathd
 import time
 import random
+
+class Canvas:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.opening_act()
+
+    def opening_act(self):
+        self.canvas.reset()
+        //specify graphic
+
+def main():
+    canvas = Canvas(unicornhathd)
